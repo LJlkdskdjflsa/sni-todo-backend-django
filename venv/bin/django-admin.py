ca9c61d9c7a4c4b23d9830d981432c98714cd057
LJlkdskdjflsa <liliangjya@gmail.com>
2021-09-25 21:17:27 +0800

    feat: aws lambda deploy

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/lj/Documents/GitHub/sni-todo-backend-django/venv/bin/python
+#!/home/lj/Documents/GitHub/sni-todo-backend-django/venv/bin/python3.8
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
